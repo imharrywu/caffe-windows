b20f9ab591c511615a4205cfaec5623e5f382ad9
harry <harry@localhost>
2018-10-26 10:14:06 +0800

    reorg src

--- windows/caffe.binding/CaffeBinding.h
+++ windows/caffe.binding/CaffeBinding.h
@@ -13,7 +13,7 @@
 
 namespace caffe {
   struct DataBlob {
-    const float* data;
+    float* data;
     std::vector<int> size;
     std::string name;
   };
@@ -23,13 +23,9 @@ namespace caffe {
     int AddNet(std::string prototxt_path, std::string weights_path, int gpu_id = 0);
     std::unordered_map<std::string, DataBlob> Forward(int net_id);
     std::unordered_map<std::string, DataBlob> Forward(std::vector<cv::Mat>&& input_image, int net_id);
-    std::unordered_map<std::string, DataBlob> Forward(std::vector<cv::Mat>& input_image, int net_id) {
-      return Forward(std::move(input_image), net_id);
-    }
+    std::unordered_map<std::string, DataBlob> Forward(std::vector<cv::Mat>& input_image, int net_id);
     void SetMemoryDataLayer(std::string layer_name, std::vector<cv::Mat>&& input_image, int net_id);
-    void SetMemoryDataLayer(std::string layer_name, std::vector<cv::Mat>& input_image, int net_id) {
-      SetMemoryDataLayer(layer_name, std::move(input_image), net_id);
-    }
+    void SetMemoryDataLayer(std::string layer_name, std::vector<cv::Mat>& input_image, int net_id);
     void SetBlobData(std::string blob_name, std::vector<int> blob_shape, float* data, int net_id);
     DataBlob GetBlobData(std::string blob_name, int net_id);
     void SetDevice(int gpu_id);
